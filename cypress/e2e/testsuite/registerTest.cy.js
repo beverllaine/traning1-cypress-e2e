f150cb6f33d44e85a9a36340568420751f1c4922
“beverllaine” <bevysanoy@gmail.com>
2024-01-14 10:51:41 +0800

    register user and assert warning message

---
+++ cypress/e2e/testsuite/registerTest.cy.js
@@ -0,0 +1,18 @@
+import { registerPage } from "../../pages/registerPage"
+const registerObj = new registerPage()
+import registerData from  '../../fixtures/registerData.json'
+
+describe('Test Automation',()=>{
+
+    it('registration flow',()=>{
+        registerObj.openURL()
+        registerObj.enterFirstName(registerData.FName_param)
+        registerObj.enterLastName(registerData.LName_param)
+        registerObj.enterEmailAdd(registerData.EmailAdd_param)
+        registerObj.enterTelephone(registerData.TelNum_param)
+        registerObj.enterPassword(registerData.Password_param)
+        registerObj.selectPolicyCheckbox()
+        registerObj.clickContinueButton()
+        registerObj.assertRegistrationWarningMessage()
+    })
+})
---
+++ cypress/pages/registerPage.js
@@ -0,0 +1,61 @@
+// This is a keyword file
+
+export class registerPage{
+
+    // Below are local variables:
+    weblocators={
+        firstName_var: '#input-firstname',
+        lastName_var: '#input-lastname',
+        email_var: '#input-email',
+        telephone_var: '#input-telephone',
+        password_var: '#input-password',
+        passwordConfirm_var: '#input-confirm',
+        policyCheckbox_var: 'input[type="checkbox"]',
+        continueButton_var: 'input[type="submit"]',
+        registrationWarningMessage_var_text: "Warning: E-Mail Address is already registered!",
+        registrationWarningMessage_var:".alert.alert-danger.alert-dismissible"
+
+    }
+
+    // Below are methods or keywords:
+
+    // param short for parameter or argument variables
+    openURL(){
+        cy.visit(Cypress.env('URL'))
+    }
+
+    enterFirstName(FName_param){
+        cy.get(this.weblocators.firstName_var).type(FName_param)
+    }
+
+    enterLastName(LName_param){
+        cy.get(this.weblocators.lastName_var).type(LName_param)
+    }
+
+    enterEmailAdd(EmailAdd_param){
+        cy.get(this.weblocators.email_var).type(EmailAdd_param)
+    }
+
+    enterTelephone(TelNum_param){
+        cy.get(this.weblocators.telephone_var).type(TelNum_param)
+    }
+
+    enterPassword(Password_param){
+        cy.get(this.weblocators.password_var).type(Password_param)
+        cy.get(this.weblocators.passwordConfirm_var).type(Password_param)
+    }
+
+    selectPolicyCheckbox(){
+        cy.get(this.weblocators.policyCheckbox_var).check()
+    }
+
+    clickContinueButton(){
+        cy.get(this.weblocators.continueButton_var).click()
+    }
+
+    assertRegistrationWarningMessage(){
+        cy.get(this.weblocators.registrationWarningMessage_var).should('be.visible')
+        cy.get(this.weblocators.registrationWarningMessage_var).contains(this.weblocators.registrationWarningMessage_var_text)
+    }
+
+}
